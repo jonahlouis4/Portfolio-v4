333ca1124b8317269b1e78389aa1831d86a75e3e
jonahlouis4 <jonah_louis@outlook.com>
2023-03-07 19:33:18 -0500

    add projects loop

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,23 @@
-import Container from "@/components/Container";
+import Container from '@/components/Container';
 
 export default function Projects() {
+  const myProjects = [
+    { name: 'StudyBuddy' },
+    { name: 'COVID-19 Tracker' },
+    { name: 'Royal Drip Check' },
+    { name: 'CreativeFloww' },
+    { name: 'iContribute' },
+  ];
+
   return (
-    <div className="pt-48">
+    <div className='pt-48'>
       <Container>
-        <h1 className="text-6xl font-extrabold drop-shadow-xl">Projects</h1>
+        <h1 className='text-6xl font-extrabold drop-shadow-xl'>Projects</h1>
+        <div className='mt-14 flex gap-14'>
+          {myProjects.map((project) => (
+            <div>{project.name}</div>
+          ))}
+        </div>
       </Container>
     </div>
   );
