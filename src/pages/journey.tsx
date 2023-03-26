90a80a9d024bf270516d7cd6418ec06f3267f2f5
jonahlouis4 <jonah_louis@outlook.com>
2023-03-26 16:17:05 -0400

    put components outside of journey

--- src/pages/journey.tsx
+++ src/pages/journey.tsx
@@ -2,33 +2,65 @@ import Container from '@/components/Container';
 import { DF_PAGE_ITEM_VARIANT, DF_WRAPPER_VARIANT } from '@/constants';
 import { motion } from 'framer-motion';
 
-export default function Journey() {
-  type ExperienceInfoProps = {
-    startDate: string;
-    endDate: string;
-    company: string;
-    description: string;
-  };
+// ========= Section Header component =========
 
-  const ExperienceInfo = ({
-    startDate,
-    endDate,
-    company,
-    description,
-  }: ExperienceInfoProps) => (
-    <div className='grid grid-cols-2 items-center py-48'>
-      <div>
-        <h2 className='text-md font-bold uppercase'>
-          {startDate} - {endDate}
-        </h2>
-        <h1 className='text-4xl font-extrabold '>{company}</h1>
-      </div>
-      <div>
-        <p className='text-2xl'>{description}</p>
-      </div>
+type SectionHeaderProps = {
+  title: string;
+};
+
+const SectionHeader = ({ title }: SectionHeaderProps) => (
+  <motion.h1
+    variants={DF_PAGE_ITEM_VARIANT}
+    className='pt-48 text-center font-extrabold text-3xl dark:text-gray-300'
+  >
+    {title}
+  </motion.h1>
+);
+
+type ExperienceInfoProps = {
+  startDate: string;
+  endDate: string;
+  company: string;
+  description: string;
+};
+
+// ========= Experience Info component =========
+
+const ExperienceInfo = ({
+  startDate,
+  endDate,
+  company,
+  description,
+}: ExperienceInfoProps) => (
+  <motion.div
+    variants={DF_PAGE_ITEM_VARIANT}
+    className='grid grid-cols-2 items-center py-48'
+  >
+    <div>
+      <motion.h2
+        variants={DF_PAGE_ITEM_VARIANT}
+        className='text-md font-bold uppercase dark:text-gray-300'
+      >
+        {startDate} - {endDate}
+      </motion.h2>
+      <h1 className='text-4xl font-extrabold dark:text-gray-300'>
+        {company}
+      </h1>
     </div>
-  );
+    <div>
+      <motion.p
+        variants={DF_PAGE_ITEM_VARIANT}
+        className='text-2xl dark:text-gray-300'
+      >
+        {description}
+      </motion.p>
+    </div>
+  </motion.div>
+);
 
+// ========= Journey page =========
+
+export default function Journey() {
   return (
     <motion.div
       variants={DF_WRAPPER_VARIANT}
@@ -50,15 +82,13 @@ export default function Journey() {
         />
 
         <>
-          <h1 className='pt-48 text-center font-extrabold text-3xl'>
-            What I've been up to lately.
-          </h1>
+          <SectionHeader title="What I've been up to lately." />
 
           {/* Experience 1 */}
           <ExperienceInfo
             startDate='June 2021'
-            endDate='June 2022'
-            company='NetFore Systems'
+            endDate='Present'
+            company='isha Foundation'
             description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Voluptas unde quibusdam vitae dolor nobis quo ut ex atque harum,
                 ullam voluptatibus ipsum saepe dolorum? Consequatur voluptate
@@ -68,7 +98,7 @@ export default function Journey() {
           <ExperienceInfo
             startDate='June 2021'
             endDate='June 2022'
-            company='iContribute'
+            company='NetFore Systems'
             description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Voluptas unde quibusdam vitae dolor nobis quo ut ex atque harum,
                 ullam voluptatibus ipsum saepe dolorum? Consequatur voluptate
@@ -77,8 +107,8 @@ export default function Journey() {
           {/* Experience 3 */}
           <ExperienceInfo
             startDate='June 2021'
-            endDate='Present'
-            company='isha Foundation'
+            endDate='June 2022'
+            company='iContribute'
             description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Voluptas unde quibusdam vitae dolor nobis quo ut ex atque harum,
                 ullam voluptatibus ipsum saepe dolorum? Consequatur voluptate
