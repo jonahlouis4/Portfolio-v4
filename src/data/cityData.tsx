0745165ff3d96d4747126c2fabd42f5899213555
jonahlouis4 <jonah_louis@outlook.com>
2023-03-28 16:30:55 -0400

    add all paris images

--- src/data/cityData.tsx
+++ src/data/cityData.tsx
@@ -36,6 +36,26 @@ import amritsar_10 from '../assets/gallery/amritsar/amritsar_10.jpg';
 import amritsar_11 from '../assets/gallery/amritsar/amritsar_11.jpg';
 import amritsar_12 from '../assets/gallery/amritsar/amritsar_12.jpg';
 
+import paris_0 from '../assets/gallery/paris/paris_0.jpg'
+import paris_1 from '../assets/gallery/paris/paris_1.jpg'
+import paris_2 from '../assets/gallery/paris/paris_2.jpg'
+import paris_3 from '../assets/gallery/paris/paris_3.jpg'
+import paris_4 from '../assets/gallery/paris/paris_4.jpg'
+import paris_5 from '../assets/gallery/paris/paris_5.jpg'
+import paris_6 from '../assets/gallery/paris/paris_6.jpg'
+import paris_7 from '../assets/gallery/paris/paris_7.jpg'
+import paris_8 from '../assets/gallery/paris/paris_8.jpg'
+import paris_9 from '../assets/gallery/paris/paris_9.jpg'
+import paris_10 from '../assets/gallery/paris/paris_10.jpg'
+import paris_11 from '../assets/gallery/paris/paris_11.jpg'
+import paris_12 from '../assets/gallery/paris/paris_12.jpg'
+import paris_13 from '../assets/gallery/paris/paris_13.jpg'
+import paris_14 from '../assets/gallery/paris/paris_14.jpg'
+import paris_15 from '../assets/gallery/paris/paris_15.jpg'
+import paris_16 from '../assets/gallery/paris/paris_16.jpg'
+import paris_17 from '../assets/gallery/paris/paris_17.jpeg'
+import paris_18 from '../assets/gallery/paris/paris_18.jpeg'
+
 export const CITIES = [
   {
     name: 'ottawa',
@@ -89,6 +109,26 @@ export const CITIES = [
   },
   {
     name: 'paris',
-    images: [ottawa_0, ottawa_1],
+    images: [
+        paris_0,
+        paris_1, 
+        paris_2, 
+        paris_3, 
+        paris_4, 
+        paris_5, 
+        paris_6, 
+        paris_7, 
+        paris_8, 
+        paris_9, 
+        paris_10, 
+        paris_11, 
+        paris_12, 
+        paris_13, 
+        paris_14, 
+        paris_15, 
+        paris_16, 
+        paris_17, 
+        paris_18,  
+    ],
   },
 ];
