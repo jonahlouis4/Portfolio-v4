abcec8e2c9653a0fd369c674442b57ff30322f07
jonahlouis4 <jonah_louis@outlook.com>
2023-03-28 14:15:43 -0400

    format

--- src/data/cityData.tsx
+++ src/data/cityData.tsx
@@ -1,72 +1,66 @@
-import ottawa_0  from '../assets/gallery/ottawa/ottawa_0.jpg'
-import ottawa_1  from '../assets/gallery/ottawa/ottawa_1.jpg'
-import ottawa_2  from '../assets/gallery/ottawa/ottawa_2.jpg'
-import ottawa_3  from '../assets/gallery/ottawa/ottawa_3.jpg'
-import ottawa_4  from '../assets/gallery/ottawa/ottawa_4.jpg'
-import ottawa_5  from '../assets/gallery/ottawa/ottawa_5.jpg'
-import ottawa_6  from '../assets/gallery/ottawa/ottawa_6.jpg'
-import ottawa_7  from '../assets/gallery/ottawa/ottawa_7.jpg'
-import ottawa_8  from '../assets/gallery/ottawa/ottawa_8.jpg'
-import ottawa_9  from '../assets/gallery/ottawa/ottawa_9.jpg'
-import ottawa_10  from '../assets/gallery/ottawa/ottawa_10.jpg'
-import ottawa_11  from '../assets/gallery/ottawa/ottawa_11.jpg'
-import ottawa_12  from '../assets/gallery/ottawa/ottawa_12.jpg'
-import ottawa_13  from '../assets/gallery/ottawa/ottawa_13.jpg'
+import ottawa_0 from '../assets/gallery/ottawa/ottawa_0.jpg';
+import ottawa_1 from '../assets/gallery/ottawa/ottawa_1.jpg';
+import ottawa_2 from '../assets/gallery/ottawa/ottawa_2.jpg';
+import ottawa_3 from '../assets/gallery/ottawa/ottawa_3.jpg';
+import ottawa_4 from '../assets/gallery/ottawa/ottawa_4.jpg';
+import ottawa_5 from '../assets/gallery/ottawa/ottawa_5.jpg';
+import ottawa_6 from '../assets/gallery/ottawa/ottawa_6.jpg';
+import ottawa_7 from '../assets/gallery/ottawa/ottawa_7.jpg';
+import ottawa_8 from '../assets/gallery/ottawa/ottawa_8.jpg';
+import ottawa_9 from '../assets/gallery/ottawa/ottawa_9.jpg';
+import ottawa_10 from '../assets/gallery/ottawa/ottawa_10.jpg';
+import ottawa_11 from '../assets/gallery/ottawa/ottawa_11.jpg';
+import ottawa_12 from '../assets/gallery/ottawa/ottawa_12.jpg';
+import ottawa_13 from '../assets/gallery/ottawa/ottawa_13.jpg';
 
-import coimbatore_0 from '../assets/gallery/coimbatore/coimbatore_0.jpg'
-import coimbatore_1 from '../assets/gallery/coimbatore/coimbatore_1.jpg'
-import coimbatore_2 from '../assets/gallery/coimbatore/coimbatore_2.jpg'
-import coimbatore_3 from '../assets/gallery/coimbatore/coimbatore_3.jpg'
-import coimbatore_4 from '../assets/gallery/coimbatore/coimbatore_4.jpg'
-import coimbatore_5 from '../assets/gallery/coimbatore/coimbatore_5.jpg'
-import coimbatore_6 from '../assets/gallery/coimbatore/coimbatore_6.jpg'
-import coimbatore_7 from '../assets/gallery/coimbatore/coimbatore_7.jpg'
+import coimbatore_0 from '../assets/gallery/coimbatore/coimbatore_0.jpg';
+import coimbatore_1 from '../assets/gallery/coimbatore/coimbatore_1.jpg';
+import coimbatore_2 from '../assets/gallery/coimbatore/coimbatore_2.jpg';
+import coimbatore_3 from '../assets/gallery/coimbatore/coimbatore_3.jpg';
+import coimbatore_4 from '../assets/gallery/coimbatore/coimbatore_4.jpg';
+import coimbatore_5 from '../assets/gallery/coimbatore/coimbatore_5.jpg';
+import coimbatore_6 from '../assets/gallery/coimbatore/coimbatore_6.jpg';
+import coimbatore_7 from '../assets/gallery/coimbatore/coimbatore_7.jpg';
 
 export const CITIES = [
-    { 
-        name: 'ottawa', 
-        images: [
-            ottawa_0,
-            ottawa_1,
-            ottawa_2,
-            ottawa_3,
-            ottawa_4,
-            ottawa_5,
-            ottawa_6,
-            ottawa_7,
-            ottawa_8,
-            ottawa_9,
-            ottawa_10,
-            ottawa_11,
-            ottawa_12,
-            ottawa_13
-        ]
-    },
-    {
-        name: 'coimbatore', 
-        images: [
-            coimbatore_0,
-            coimbatore_1,
-            coimbatore_2,
-            coimbatore_3,
-            coimbatore_4,
-            coimbatore_5,
-            coimbatore_6,
-            coimbatore_7,
-        ]
-    },
-    {
-        name: 'amritsar', 
-        images: [
-            ottawa_0,
-            ottawa_1
-        ]
-    },
-    {
-        name: 'paris', 
-        images: [
-            ottawa_0,
-            ottawa_1
-        ]
-    },
-]
+  {
+    name: 'ottawa',
+    images: [
+      ottawa_0,
+      ottawa_1,
+      ottawa_2,
+      ottawa_3,
+      ottawa_4,
+      ottawa_5,
+      ottawa_6,
+      ottawa_7,
+      ottawa_8,
+      ottawa_9,
+      ottawa_10,
+      ottawa_11,
+      ottawa_12,
+      ottawa_13,
+    ],
+  },
+  {
+    name: 'coimbatore',
+    images: [
+      coimbatore_0,
+      coimbatore_1,
+      coimbatore_2,
+      coimbatore_3,
+      coimbatore_4,
+      coimbatore_5,
+      coimbatore_6,
+      coimbatore_7,
+    ],
+  },
+  {
+    name: 'amritsar',
+    images: [ottawa_0, ottawa_1],
+  },
+  {
+    name: 'paris',
+    images: [ottawa_0, ottawa_1],
+  },
+];
