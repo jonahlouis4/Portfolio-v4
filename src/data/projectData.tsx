74b48ad82bb9700b33ed53a926e0e2973fb3817f
jonahlouis4 <jonah_louis@outlook.com>
2023-04-10 14:21:25 -0400

    update project wording

--- src/data/projectData.tsx
+++ src/data/projectData.tsx
@@ -4,7 +4,7 @@ export const PROJECTS = [
     name: 'StudyBuddy',
     description: 'Flashcard web application designed for studying.',
     longDescription:
-      'A simple flashcard web application designed for studying. Provides the ability to add an extensive range of questions into an account and includes a randomized order for each quiz.',
+      'A simple flashcard web application designed for studying. It allows adding an extensive range of questions into an account and includes a randomized order for each quiz.',
     langs: ['React', 'TailwindCSS', 'Firebase', 'Framer Motion'],
     media: null,
     links: [{ github: 'https://github.com/jonahlouis4/StudyBuddy' }],
@@ -15,7 +15,7 @@ export const PROJECTS = [
     name: 'COVID-19 Tracker',
     description: 'Live COVID-19 web application tracker.',
     longDescription:
-      'A global scale covid-19 tracker of deaths, cases, and recoveries using a COVID-19 API. Includes sorted charts and summaries from a wide variety of Countries.',
+      'A global scale covid-19 tracker of deaths, cases, and recoveries using a COVID-19 API. It includes sorted charts and summaries from a wide variety of Countries.',
     langs: ['React', 'GraphQL', 'Apollo Client', 'Ant Design', 'AntV'],
     media: null,
     links: [{ github: 'https://github.com/jonahlouis4/COVID19-Tracker' }],
@@ -26,7 +26,7 @@ export const PROJECTS = [
     name: 'Royal Drip Check',
     description: 'Streetwear reselling e-commerce website.',
     longDescription:
-      'A shopify e-commerce website used to resell the latest streetwear from a local business in Ottawa, Canada. Anything from shoes, t-shirts, hoodies, sweatshirts, or pants.',
+      'A Shopify e-commerce website that resells the latest streetwear from a local business in Ottawa, Canada. Anything from shoes, t-shirts, hoodies, sweatshirts, or pants.',
     langs: ['Shopify', 'TailwindCSS'],
     media: null,
     links: [{ website: 'https://royaldripcheck.ca/' }],
